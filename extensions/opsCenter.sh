a9da52a2a62cd05a1f8015acba8494ceebd6ab5d
Jidesh <jidesh.gopalan@reachlocal.com>
2016-07-21 14:48:45 +0530

    update opscenter location

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 cloud_type="azure"
-location=$1 #this is the location of the seed, not necessarily of this node
+seed_node_location=$1
 unique_string=$2
 
-seed_node_dns_name="dc0vm0$unique_string.$location.cloudapp.azure.com"
-
-echo "Configuring nodes with the settings:"
+echo "Input to node.sh is:"
 echo cloud_type $cloud_type
-echo location $location
+echo seed_node_location $seed_node_location
 echo unique_string $unique_string
+
+seed_node_dns_name="dc0vm0$unique_string.$seed_node_location.cloudapp.azure.com"
+
+echo "Calling opscenter.sh with the settings:"
+echo cloud_type $cloud_type
 echo seed_node_dns_name $seed_node_dns_name
 
 wget https://github.com/jideshgopalan/install-datastax/archive/master.zip
@@ -18,4 +21,3 @@ unzip master.zip
 cd install-datastax-master/bin
 
 ./opscenter.sh $cloud_type $seed_node_dns_name
-
